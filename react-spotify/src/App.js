48415667625be151261bff242b4abc4879cfb5ff
abhiyogacs07@gmail.com <abhiyogacs07@gmail.com>
2022-04-04 07:49:40 +0800

    membenarkan components

--- react-spotify/src/App.js
+++ react-spotify/src/App.js
@@ -1,80 +1,12 @@
-import {useEffect, useState} from "react";
+import SearchRes from './components/search';
 import './App.css';
-import axios from 'axios';
 
 function App() {
-    const CLIENT_ID = "ccbf3bbbcbb9487fadc191d006ee678a"
-    const REDIRECT_URI = "http://localhost:3000/"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
-    const SCOPE = "playlist-modify-private"
-
-    const [token, setToken] = useState("")
-    const [searchKey, setSearchKey] = useState("")
-    const [Tracks, setTracks] = useState([])
-
-    const parseToken = (url) =>{
-        const parsed = url.split('&')[0].split('=')
-        const token = parsed[parsed.length-1] ?? null
-        setToken(token)
-    }
-
-    useEffect(()=>{
-        if (window.location.hash) parseToken(window.location.hash)
-    },[])
-
-    const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
-    }
-
-    const searchTracks = async (e) => {
-        e.preventDefault()
-        const {data} = await axios.get("https://api.spotify.com/v1/search", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            params: {
-                q: searchKey,
-                type: "track"
-            }
-        })
-
-        setTracks(data.tracks.items)
-    }
-
-    const renderTracks = () => {
-        return Tracks.map(track => (
-            <div className="track" key={track.id}>
-                {track.album.images.length ? <img src={track.album.images[1].url} alt=""/> : <div>No Image</div>}
-                <p className="title">Track:</p>
-                <h5>{track.name}</h5>
-                <p className="title">Artist:</p>
-                <h5>{track.artists[0].name}</h5>
-            </div>
-        ))
-    }
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Spotify React</h1>
-                {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Login
-                        to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
-
-                {token ?
-                    <form onSubmit={searchTracks}>
-                        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-                        <button type={"submit"}>Search</button>
-                    </form>
-
-                    : <h2>Please login</h2>
-                }
-
-                {renderTracks()}
-
+            <SearchRes />
             </header>
         </div>
     );
---
+++ react-spotify/src/compoonents/search/index.js
@@ -0,0 +1,89 @@
+ import { useEffect, useState } from 'react'
+import Container from "../Container";
+import axios from 'axios'
+import { useSearchResult } from '../../context/useSearchResult';
+import { useStoreApi } from '../../context/useStoreApi';
+
+const CLIENT_ID = "ccbf3bbbcbb9487fadc191d006ee678a"
+const BASE_URL = "https://api.spotify.com/v1/"
+const REDIRECT_URI = "http://localhost:3000/"
+const AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+const SCOPE = "playlist-modify-private"
+
+
+const SearchRes = () => {
+    const [token, setToken] = useState("")
+    const [searchKey, setSearchKey] = useState("")
+    const [Tracks, setTracks] = useState([])
+
+    const parseToken = (url) =>{
+        const parsed = url.split('&')[0].split('=')
+        const token = parsed[parsed.length-1] ?? null
+        setToken(token)
+    }
+
+    useEffect(()=>{
+        if (window.location.hash) parseToken(window.location.hash)
+    },[])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+}
+
+    const searchTracks = async (e) => {
+        e.preventDefault()
+        const {data} = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchKey,
+                type: "track"
+            }
+        })
+
+        setTracks(data.tracks.items)
+    }
+
+    const renderTracks = () => {
+        return Tracks.map(track => (
+            <div className="track" key={track.id}>
+                {track.album.images.length ? <img src={track.album.images[1].url} alt=""/> : <div>No Image</div>}
+                <p className="title">Track:</p>
+                <h5>{track.name}</h5>
+                <p className="title">Artist:</p>
+                <h5>{track.artists[0].name}</h5>
+            </div>
+        ))
+    }
+
+    return (
+        <section className="bg-gray-800 py-4">
+            <Container>
+                <div className="flex flex-col md:flex-row items-start md:items-center justify-between px-2">
+                    <h1>Spotify React</h1>
+                    {!token ?
+                        <a href={`${AUTHORIZE_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=${SCOPE}`}>Login
+                            to Spotify</a>
+                        : <button onClick={logout}>Logout</button>}
+
+                    {token ?
+                        <form onSubmit={searchTracks}>
+                            <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+                            <button type={"submit"}>Search</button>
+                        </form>
+
+                        : <h2>Please login</h2>
+                    }
+
+                    {renderTracks()}
+
+                </div>
+            </Container>
+        </section>
+        
+    )
+}
+
+export default SearchRes;
